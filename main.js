bba6a870fec7028e6d3e00c521624d27b9963435
HyoKim <hyo@tapasmedia.co>
2016-03-09 17:52:01 +0900

    Privacy

--- main.js
+++ main.js
@@ -1,37 +1,25 @@
 var express = require('express');
 var app = express();
 
+
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-  var ua = req.headers['user-agent'];
-
-  console.log(__origin(ua))
-
   res.sendFile(__dirname + '/' + 'index.html');
+});
 
-  //app.render('index', {name: 'name'}, function (err, html) {
-  //  console.log('render')
-  //});
+app.get('/about', function (req, res) {
+  res.sendFile(__dirname + '/views/' + 'about.html');
+});
+
+app.get('/help', function (req, res) {
+  res.sendFile(__dirname + '/views/' + 'help.html');
+});
+
+app.get('/privacy', function (req, res) {
+  res.sendFile(__dirname + '/views/' + 'privacy.html');
 });
 
 app.listen(8082, function (){
   console.log('start server');
 });
-
-
-var __origin = function (ua) {
-  var $ = {};
-
-  if (/like Mac OS X/.test(ua)) {
-    $.iOS = /CPU( iPhone)? OS ([0-9\._]+) like Mac OS X/.exec(ua)[2].replace(/_/g, '.');
-    $.iPhone = /iPhone/.test(ua);
-    $.iPad = /iPad/.test(ua);
-  } else if (/Android/.test(ua)) {
-    $.Android = /Android ([0-9\.]+)[\);]/.exec(ua)[1];
-  } else {
-    $.Web = true;
-  }
-
-  return $;
-};

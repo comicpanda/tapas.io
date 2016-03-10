1c51a1e42a0d69e3c9aebdbd416c559ef9de6f41
HyoKim <hyo@tapasmedia.co>
2016-03-10 17:36:39 +0900

    Social link

--- main.js
+++ main.js
@@ -7,10 +7,6 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/' + 'index.html');
 });
 
-app.get('/about', function (req, res) {
-  res.sendFile(__dirname + '/views/' + 'about.html');
-});
-
 app.get('/help', function (req, res) {
   res.sendFile(__dirname + '/views/' + 'help.html');
 });
